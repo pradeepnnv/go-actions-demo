1521accb277539ec68edd9b90a0e0b893f245c10
pradeepnnv <pradeepnnv@gmail.com>
2023-02-25 22:25:00 -0600

    add proverb
    Signed-off-by: pradeepnnv <pradeepnnv@gmail.com>

--- src/hello.go
+++ src/hello.go
@@ -1,5 +1,14 @@
 package goactionsdemo
 
+import (
+	"rsc.io/quote"
+	quoteV3 "rsc.io/quote/v3"
+)
+
 func Hello() string {
-	return "Hello World!"
+	return quote.Hello()
+}
+
+func Proverb() string {
+	return quoteV3.Concurrency()
 }
